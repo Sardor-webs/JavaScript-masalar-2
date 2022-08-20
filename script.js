bbc40be6080a17d6ec8bdf515a91025c7823f383
Sardor-webs <sardorrixsiboevv@gmail.com>
2022-08-20 13:42:54 +0500

    JavaScript-masalar-2

---
+++ script.js
@@ -0,0 +1,158 @@
+// //==============={1-masala}================
+// for (let k = 1; k <= 6; k++) {
+// console.log(1);
+// }
+
+
+
+// // =============={2-masala}================
+// for (let a = 2; a<= 13; a++) {
+// console.log(a);
+// }
+
+
+
+// // =============={3-masala}=================
+// for (let a= 8; a> 1; a--) {
+//     if(a=8) continue
+// console.log(a);
+// }
+
+
+// // =============={4-masala}=================
+// for (let i = 1; i <=10; i++) {
+//     console.log(`${i}kg konfet ${i*10000} so'm`);
+// }
+
+
+// // =============={5-masala}=================
+// for (let i = 1; i <=10; i++) {
+//     console.log(`${i/10}kg konfet ${i*1000} so'm`);
+// }
+
+// // =============={6-masala}=================
+// for (let i = 10; i <=20; i++) {
+//     if(i%2 >0) continue
+//     console.log(`${i/10}kg konfet ${i*1000} so'm`);
+// }
+
+
+
+// // =============={7-masala}==================
+// let num1 = prompt('Qaysi sondan')*1;
+// let num = prompt('Qaysi songacha')*1;
+// let sum = 0;
+// for (let i=num1 ; i<=num ; i++){
+//     sum+=i
+// }
+// alert(sum);
+
+
+// // =============={8-masala}===================
+// let num1 = prompt('Qaysi sondan')*1;
+// let num = prompt('Qaysi songacha')*1;
+// let sum = 1;
+// for (let i=num1 ; i<=num ; i++){
+//     sum*=i
+// }
+// alert(sum);
+
+
+// // =============={9-masala}====================
+// let num1 = prompt('Qaysi sondan')*1;
+// let num = prompt('Qaysi songacha')*1;
+// let sum = 0;
+// for (let i=num1 ; i<=num ; i++){ 
+//     sum= sum + (i*i)
+// }
+// alert(sum);
+
+
+// // =============={10-masala}====================
+// let n = 4 ;
+// let sum = 0;
+// for(let i=1 ; i<=n ; i++){
+// sum = sum + (1/i)
+// }
+// console.log(sum);
+
+// // =============={11-masalsa}====================
+// let num1 = 11;
+// let num2 = 33;
+// for(let i=num1; i<=num2; i++){
+//     if(i%2==0) continue
+//     console.log(i);
+// }
+
+
+// // ==============={12-masala}====================
+// let num1 = 11;
+// let num2 = 33;
+// for(let i=num1; i<=num2; i++){
+//     if(i%2>0) continue
+//     console.log(i);
+// }
+
+
+
+
+// // ==============={13-masala}====================
+// let n = 100;
+// for (let i = 1; i <= n; i++) {
+//     let count = 0;
+//     for (let j = 1; j <= i; j++) {
+//         if (i%j == 0)
+//             count++
+//     }
+//     if (count==2) {
+//         } else {
+//             console.log(i)
+//         }
+// }
+
+
+
+// // ==============={14-masala}=====================
+// let n = 10;
+// for(let i=1 ; i<=n; i++){
+//     console.log(i*i);
+// }
+
+
+// // ==============={15-masala}=====================
+// let n=26;
+// for(let i=n; i>=1; i--){
+//     console.log(i);
+// }
+
+
+
+
+// // ================{16-masala}====================
+// let n =100;
+// for(let i=1; i<=100; i++){
+//     if(i%5==0)
+//     console.log(i);
+// }
+
+
+// // ================{17-masala}====================
+// let n = 10,
+//     result = 1;
+// for(let i=1; i<=n; i++){
+//     result*=i;
+// }
+// console.log(result);
+
+
+// // ================{18-masala}=====================
+// let n = 5,
+//     result = 1,
+//     sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i; j++){
+//         result*=j
+        
+//     }
+// }
+// console.log(result);
